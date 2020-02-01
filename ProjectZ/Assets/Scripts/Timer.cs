e8a9998fbf1a0c9be086e3deb322c0f4b07acc00
Angela Lin <40971579+k95258@users.noreply.github.com>
2020-02-01 20:45:40 +0800

    時間條

---
+++ ProjectZ/Assets/Scripts/Timer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    Image TimerBar;
+    public float Maxtime = 5f;
+    float TImeLeft;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TimerBar = GetComponent<Image>();
+        TImeLeft = Maxtime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (TImeLeft > 0)
+        {
+            TImeLeft -= Time.deltaTime;
+            TimerBar.fillAmount = TImeLeft / Maxtime;
+        }
+
+        else
+        {
+            Time.timeScale = 0;
+        }
+  
+    }
+}
